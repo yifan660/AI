6f1a34d498816679cd02c62480528ee209e66c52
yifan660 <yifan660@outlook.com>
2020-10-21 13:53:46 +0800

    add type.c file

--- learning/cnn/cnn.c
+++ learning/cnn/cnn.c
@@ -7,19 +7,28 @@ int main()
 
 }
 
-cnn_memset()
+
+create_datatype()
+{
+	// function to allocate memory for data type
+	sbrk();	
+}
+
+void cnn_memset()
 {
 	if()	{
+		// record info in 
 		mem[i-5] = info;
-		size+=10;
+		size+=num*sizeof(var);
+		mem[	
 		sblock = 100;
+		size = 1000;
 	} else if()	{
-		sbrk;		
+		sbrk(size);		
 	}
 }
 
-cnn_malloc()
+(void *)cnn_malloc()
 {
 	cnn_memset();
-	cnn_memset();
-}
+
